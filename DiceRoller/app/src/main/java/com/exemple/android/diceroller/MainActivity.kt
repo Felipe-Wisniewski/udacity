8cfdae8d66a7acc2949ab2ee9a14f817b3af3811
Felipe Wisniewski <felipewisniewski@gmail.com>
2020-03-25 10:29:54 -0300

    exercise - app DiceRoller - first app Android Componets

---
+++ DiceRoller/app/src/main/java/com/exemple/android/diceroller/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.exemple.android.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_roll.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        imgDice.setImageResource(drawableResource)
+    }
+}
